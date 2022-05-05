82a4d88d4f75c763aa807d9820333bb574397ada
Ganesh.Rajamani <ganesh.rajamani@smarteinc.com>
2022-05-05 14:33:10 +0530

    Shopping cart first phase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
+import Header from './header';
+import Item from './item';
+import Cart from './cart'
 
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+export const DeleteContext = React.createContext()
 function App() {
+  const [items, setItems] = useState([{
+    sno: null,
+    itemName: "",
+    price: ""
+  }])
+
+
+  const addItem = (itemName, price) => {
+
+    console.log("Adding the cart", itemName, price)
+    const item = {
+      sno: Math.ceil(Math.random() * 10),
+      itemName: itemName,
+      price: "$" + price
+    }
+    console.log(item)
+    setItems([...items, item])
+  }
+  const deleteItem = (item) => {
+    console.log("Deleting", item)
+    console.log("Index",items.indexOf(item))
+    setItems(items.filter(e=>{
+    return e!==item
+    }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div>
+        <UserContext.Provider value={addItem}>
+          <Item />
+        </UserContext.Provider>
+      </div>
+      <div>
+        <ChannelContext.Provider value={items}>
+          <DeleteContext.Provider value={deleteItem}>
+            <Cart />
+          </DeleteContext.Provider>
+        </ChannelContext.Provider>
+      </div>
+    </>
   );
 }
 
